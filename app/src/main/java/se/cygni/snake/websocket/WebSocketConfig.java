b63d5a65de806733d0825d3044c740308895845c
Emil Breding <emil.breding@gmail.com>
2016-01-12 16:45:56 +0100

    Added echo websocket and testclient

---
+++ app/src/main/java/se/cygni/snake/websocket/WebSocketConfig.java
@@ -0,0 +1,27 @@
+package se.cygni.snake.websocket;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import se.cygni.snake.websocket.echo.EchoWebSocketHandler;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(echoWebSocketHandler(), "/echo").withSockJS();
+//        registry.addHandler(snakeWebSocketHandler(), "/snake").withSockJS();
+    }
+
+    @Bean
+    public WebSocketHandler echoWebSocketHandler() {
+        return new EchoWebSocketHandler();
+    }
+}
