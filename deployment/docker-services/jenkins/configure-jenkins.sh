7922720c535ae84bd3d4ab50a54887399f724e15
Emil Breding <emil.breding@gmail.com>
2016-02-26 22:43:30 +0100

    fixed jenkins config

--- deployment/docker-services/jenkins/configure-jenkins.sh
+++ deployment/docker-services/jenkins/configure-jenkins.sh
@@ -3,8 +3,7 @@
 echo "Adding plugins and Jenkins configuration..."
 
 source "$snake_defaults"
-rm -rf "$docker_base_path/jenkins"
-cp docker-services/jenkins/jenkins.tgz "$docker_base_path/"
-tar zxvf "$docker_base_path/jenkins.tgz" "$docker_base_path/"
-chown -R 1000:1000 "$docker_base_path/jenkins"
-rm -rf "$docker_base_path/jenkins.tgz"
+rm -rf "$JENKINS_DATA_DIR"
+tar zxvf docker-services/jenkins/jenkins.tgz -C "$docker_base_path/"
+chown -R 1000:1000 "$JENKINS_DATA_DIR"
+chmod 777 "$JENKINS_DATA_DIR"
